d685feb810e8a5f9e82b8397e82fd6e324f99c72
QusaiTA <tashtoshqusai@gmail.com>
2021-08-09 00:10:57 +0300

    removing comments

--- src/component/content.js
+++ src/component/content.js
@@ -3,14 +3,7 @@ import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
 export class Content extends Component {
-  /**
-   * THis is going to be a component requesting the /verify-token endpoint form our backend
-   * to verify if the users token is valid or not
-   * 
-   * TODO:
-   * 1. the moment this component load, we will send the request over to the backend
-   * 2. you will send in the request header the authorization token that we will get from auth0
-   */
+  
 
   constructor(props) {
     super(props);
@@ -21,16 +14,10 @@ export class Content extends Component {
   }
 
   componentDidMount = () => {
-    // the function getIdTokenClaims is a promise based function
     this.props.auth0.getIdTokenClaims().then(tokenResponse => {
 
       const jwt = tokenResponse.__raw;
-      // console.log(tokenResponse);
-      // console.log(jwt);
-
-      // once we got the token, now we can send token in the headers with our request to our backend URL
-
-      // to send tokens in the headers with axios, we will create a config object
+ 
 
       const config = {
         headers: {
@@ -42,7 +29,6 @@ export class Content extends Component {
       };
 
       axios(config).then((axiosResponse) => {
-        // console.log('backend data', axiosResponse.data);
 
         this.setState({
           user: axiosResponse.data
